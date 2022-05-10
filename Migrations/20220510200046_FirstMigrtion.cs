0be43017afe86fb60d5445fdee3e3b3edaa8f83e
radisa11 <zlatic66@.com>
2022-05-10 16:48:50 -0500

    First commit

---
+++ Migrations/20220510200046_FirstMigrtion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace opet.Migrations
+{
+    public partial class FirstMigrtion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Music",
+                columns: table => new
+                {
+                    MusicId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: false),
+                    Artist = table.Column<string>(nullable: false),
+                    Genre = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Music", x => x.MusicId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Music");
+        }
+    }
+}
---
+++ Models/Music.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+
+namespace opet.Models
+{
+    public class Music
+    {
+        [Key]
+        public int MusicId {get;set;}
+        [Required]
+        public string Title {get;set;}
+        [Required]
+        public string Artist {get;set;}
+        [Required]
+        public string Genre {get;set;}
+        public DateTime CreatedAt {get;set;} = DateTime.Now;
+        public DateTime UpdatedAt {get;set;} = DateTime.Now;
+    }
+}
